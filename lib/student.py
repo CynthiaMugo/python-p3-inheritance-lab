81e945cacb436685d4f47b9a2b418e4c5accdf8f
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-08-12 01:49:58 +0300

    print a single string

--- lib/student.py
+++ lib/student.py
@@ -7,6 +7,13 @@ class Student(User):
         super().__init__(first_name, last_name)
         self.knowledge = []
     
-    def learn(self, new_knowledge):
+    def learn(self, new_knowledge: str):
         self.knowledge.append(new_knowledge)
-        
+        return self.knowledge
+
+s1 = Student(first_name="Collins", last_name="Joash")
+print(s1.first_name)
+print(s1.last_name)
+
+s1.learn("I am learning append function in Python") 
+print(s1.knowledge[-1]) 
